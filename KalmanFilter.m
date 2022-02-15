e6a4780ecdf714fda9a2fb1bcdafc71a5a3e2d9d
Matias-Smith <75107250+Matias-Smith@users.noreply.github.com>
2022-02-14 18:55:39 -0800

    Update KalmanFilter.m
    I think 99% of code is written out, just needs to be troubleshot and polished up

--- KalmanFilter.m
+++ KalmanFilter.m
@@ -17,44 +17,51 @@ V = [0.3, 0.6, 0.5, 0.4, 0.5, 0.3, 0.6, 0.4]
 t = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 t = [0:length-1];
 
-% xk1_k1 = (value);
-% uk1 = (value);
+xhatk_1 = [SOC; Vc];
+%I is measured
 
-% Pk1_k1 = (value);
-% Aprime_k1 = (matrix);
-% Eprime_k1 = (matrix);
-% Qk1 = (value or matrix);
+Aprime1 = [1, 0; 0, -T/(Cc*Rc)]     %A' 
+Cprime = [2.04, 0]                  %VOC(SOC);
+Eprime1 = [1, 0; 0, 1]              %E'
+Fprime1 = [1, 0; 0, 1]              %F'
+Rk = 1E-4;
+
+Qk1 = [2.5*10^(-7), 0; 0, 0]
+
+xhat = zeros(2, n)                  %Initializing 
+xhat(1) = 1;
+P = zeros(2, 2, n);
+P(1:2, 1:2, 1) = Rk * eye(2);       %P starts of as n 2x2 identity matricies
 
 %--------------------------------------------------------------------------
 % At time k
 %--------------------------------------------------------------------------
 
-k = k+1;
+VOC = @(SOC)                                      %Need equation for voc in terms of SOC 
 
-% fk(xk1_k1, uk1, 0) = (function definition);
+yk = @(V, Voc0) V - Voc0;                         %yk: Actual Voltage
 
-% Cprime_k = (matrix);
-% Fprime_k = (matrix);
-% Rk = (value);
+hk = @(xhat, I) VOC(xhat(1, 1)) - R0*I;           %hk: Cacluated Voltage?
 
-% yk = (value or matrix);
-% uk = (value);
-% hk(xk_k1, uk, 0) = (function definition);
-
-Aprime_k1 = [1, 0; 0, -T/(Cc*Rc)]
-Eprime_k1 = [1, 0; 0, 1]
-Fprime_k1 = [1, 0; 0, 1]
-
-Qk1 = [2.5*10^(-7), 0; 0, 0]
+fk = @(xhatk_1, I) xhatk_1 + dt * [-I / Cbat; (I / Ccap)  - (Vc / (Ccap * Rc))];  %xhat = previous xhat + change in xhat in time: dt     %XHAT IS A DERIVATIVEEEE
 
 
-function [xk_k, Pk_k] = EKF(xk1_k1, uk1, Pk1_k1, )
+%Iterate for each sample
+for i = 0:n                                               %Not sure if this I is offset
+    [xhat(:, i+1), P(:, :, i+1)] = EKF(xhat(:, i), P(:, :, i), I+1, I, V, Voc0, Rk, Aprime, Cprime, Eprime, Fprime)
 
-    xk_k1 = fk(xk1_k1, uk1, 0);
-    Pk_k1 = Aprime_k1 * Pk1_k1 * transpose(Aprime_k1) + Eprime_k1 * Qk1 * transpose(Eprime_k1);
-    Lk = Pk_k1 * transpose(Cprime_k) * inv(Cprime_k * Pk_k1 * transpose(Cprime_k) + Fprime_k * Rk * transpose(Fprime_k));
-    xk_k = xk_k1 + Lk * (yk - hk(xk_k1, uk, 0));
-    Pk_k = Pk_k1 - Lk * Cprime_k * Pk_k1;
+
 end
 
-function 
+
+
+%Calculate next xhat and P using the previous ones
+function [xhatCorrected, PCorrected] = EKF(xhatk_1, Pk_1, I, Ik_1 , V, Voc0, Rk, Aprime, Cprime, Eprime, Fprime)
+
+    xhat = fk(xhatk_1, Ik_1, 0);
+    P = Aprime * Pk_1 * Aprime.' + Eprime * Qk1 * Eprime.';
+    Lk = P * Cprime.' * inv(Cprime * Pk_1 * Cprime.' + Fprime * Rk * Fprime.');
+    xhatCorrected = xhat + Lk * (yk(V, Voc0) - hk(xhat, I));
+    PCorrected = P - Lk * Cprime * Pk_1;
+end
+
